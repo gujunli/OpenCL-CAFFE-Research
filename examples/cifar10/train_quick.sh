026621f557910c9d804640d2bd0eb8f3c0132c36
Junli Gu <junli.gu@amd.com>
2015-01-29 18:26:39 +0800

    This is to check in the prototxt files and *sh files for training

---
+++ examples/cifar10/train_quick.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+TOOLS=../../build/tools
+
+#LOG(INFO) << "file"
+#   FLAGS_logtostderr = 0
+#  FLAGS_log_dir = "/home/jlgu/Documents"
+
+GLOG_logtostderr=0 $TOOLS/train_net.bin cifar10_quick_solver.prototxt
+#$TOOLS/train_net.bin cifar10_quick_solver.prototxt
+
+
+#reduce learning rate by fctor of 10 after 8 epochs
+#GLOG_logtostderr=1 $TOOLS/train_net.bin cifar10_quick_solver_lr1.prototxt cifar10_quick_iter_4000.solverstate
